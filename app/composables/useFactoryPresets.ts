6398bd2968e76f207cca0985e0ca00c11923e647
rich-tribeweb <rich@tribeweb.co.uk>
2025-08-25 21:59:29 +0100

    chore: refactor to use composable

---
+++ app/composables/useFactoryPresets.ts
@@ -0,0 +1,21 @@
+export function getRelatedPresets(
+  model: Partial<keyof Schema['data']['tone']> = 'THRGroupAmp',
+  key: Partial<keyof THRGroupAmp> | Partial<keyof THRGroupCab> = '@asset',
+  value: THRGroupAmp[Partial<keyof THRGroupAmp>] | THRGroupCab[Partial<keyof THRGroupCab>] = 'THR10C_Deluxe',
+  factoryPresets: Schema[]
+) {
+  return (factoryPresets as Schema[])?.filter((preset: Schema) =>
+    (preset.data.tone[model] as { [key: string]: THRGroupAmp[Partial<keyof THRGroupAmp>] | THRGroupCab[Partial<keyof THRGroupCab>] })[key] === value)
+}
+
+export function useFactoryPresets() {
+  const { data: factoryPresets } = useAsyncData('factoryPresets', () => {
+    return queryCollection('thrPresets')
+      .select('data')
+      .all()
+  })
+  return {
+    factoryPresets,
+    getRelatedPresets
+  }
+}
