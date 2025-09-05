236851fabb497fdacbed5a2c4a3671ec56b9fcc3
rich-tribeweb <rich@tribeweb.co.uk>
2025-09-05 23:55:12 +0100

    feat: sortable presets

---
+++ app/components/content/PresetList.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { useSortable } from '@vueuse/integrations/useSortable'
+
+const { factoryPresets } = useFactoryPresets()
+
+const el = useTemplateRef<HTMLElement>('el')
+
+useSortable(el, factoryPresets as Ref<Schema[]>, {
+  animation: 400
+})
+</script>
+
+<template>
+  <div ref="el" class="columns-1 sm:columns-2 md:columns-3 gap-2">
+    <UBadge
+      v-for="(item, i) in factoryPresets"
+      :key="i"
+      class="m-1 cursor-grab"
+      variant="solid"
+      size="lg"
+      trailing-icon="i-lucide-grip-vertical"
+    >
+      <template #leading>
+        <span class="w-5">{{ i }}.</span>
+      </template>
+      <UInput v-model="item.data.meta.name" />
+    </UBadge>
+  </div>
+</template>
